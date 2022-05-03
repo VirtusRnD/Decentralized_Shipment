b6c7919995938c62f900427088353d574f4b588e
hasanaliozkan-dev <hasanaliozkan-dev@outlook.com>
2022-05-03 13:45:50 +0300

    "Coustomer contract is writing "

--- contracts/Customer.sol
+++ contracts/Customer.sol
@@ -1,15 +1,28 @@
 pragma solidity ^0.5.0;
 import 'contracts/ShipmentDapp.sol';
+import 'contracts/ShipmentDapp.sol';
 
 
 contract Customer{
 
-    string addressOfCustomer;
+    string shipmentAddressOfCustomer;
     string name;
 
-    constructor(string memory _addressOfCustomer, string memory _name) public {
-        addressOfCustomer = _addressOfCustomer;
+    constructor(string memory _shipmentAddressOfCustomer, string memory _name) public {
+        shipmentAddressOfCustomer = _shipmentAddressOfCustomer;
         name = _name;
+    }
+
+    function order(Product memory _product) public payable{
+        orderId++;
+        return Order(orderId, "order1", _product, OrderStatus.PREPARING, false);
+    }
+
+    function checkOrderStatus(uint _orderId) public view returns (OrderStatus){
+        return Order(_orderId).productStatus;
+    }
+    function cancelTheOrder(uint _orderId) public {
+        Order(_orderId).productStatus = OrderStatus.CANCELLED;
     }
 
 
--- contracts/ShipmentDapp.sol
+++ contracts/ShipmentDapp.sol
@@ -2,10 +2,14 @@ pragma solidity ^0.5.0;
 
 contract ShipmentDapp {
 
+    uint productId = 0;
+    uint orderId  = 0;
+    
     enum OrderStatus {
         PREPARING,
         ONTHEWAY,
-        RECEIVED
+        RECEIVED,
+        CANCELLED
     }
     struct Product{
         uint id;
@@ -17,11 +21,10 @@ contract ShipmentDapp {
     struct Order{
         uint OrderId;
         string orderName;
-        Product[] product;
-        OrderStatus prductStatus;
+        Product product;
+        OrderStatus orderStatus;
         bool isDamaged;
     }
-    
 
 
 }
