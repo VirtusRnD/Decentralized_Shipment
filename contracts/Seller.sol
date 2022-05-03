b4e15b51c3beb1a7e2d74ac4102b631fd97cf2ae
hasanaliozkan-dev <hasanaliozkan-dev@outlook.com>
2022-05-03 19:28:26 +0300

    "Seller.sol is updated"

--- contracts/Seller.sol
+++ contracts/Seller.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 import 'contracts/ShipmentDapp.sol';
 
 contract Seller  {
@@ -6,13 +7,27 @@ contract Seller  {
     //This is the ID of the product and it's unique.
     //It is incremented every time a new product is added.
     uint productId = 0;
-
+    uint orderId  = 0;
+    
+    enum OrderStatus {
+        PREPARING,
+        ONTHEWAY,
+        RECEIVED,
+        CANCELLED
+    }
     //Product is a struct which contains the product details.
     struct Product{
         uint id;
         string name;
         uint price;
         uint quantity;
+    }
+    struct Order{
+        uint OrderId;
+        string orderName;
+        Product product;
+        OrderStatus orderStatus;
+        bool isDamaged;
     }
 
     constructor() public {
@@ -30,11 +45,22 @@ contract Seller  {
         return Product(productId, _name ,_price,_quantity); //Create product and return it.
 
     }
+    //This function is be used to checking order status.
+    function checkTheOrder(Order memory _order) public returns(OrderStatus) {
+        return _order.orderStatus;
+    }
+
     // This function is used to receive order.
     function receiveOrder(Product memory _product, string memory _orderName) public returns(Order memory){
         return Order(1, "order1", _product, OrderStatus.PREPARING, false);
 
     }
+    function transferOrder2Shipment(Order memory _order) public {
+            _order.orderStatus = OrderStatus.ONTHEWAY;
+    }
+
+
+    
 }
 
 
---
+++ contracts/ShipmentComp.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+import 'contracts/ShipmentDapp.sol';
+import 'contracts/Seller.sol';
+contract ShipmentComp {
+
+    mapping(uint => Order) orders;
+
+
+    function signOrderDamaged(uint _orderId) public {
+        orders[orderId].isDamaged = true;
+    }
+
+    function transferOrder2Customeruint(uint _orderId) public {
+        require(orders[_orderId].isDamaged == false);
+        require(orders[_orderId].orderStatus == OrderStatus.ONTHEWAY);
+        orders[_orderId].orderStatus = OrderStatus.RECEIVED;
+        orders.
+    }
+
+
+
+}
